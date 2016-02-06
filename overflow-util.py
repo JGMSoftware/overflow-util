5d878ddba103a71a79b984410dbefa7d0667776a
Allan Brown <allanbrown239@gmail.com>
2016-02-06 16:24:34 +0000

    Added functions for converting ASCII to hex and vice versa.

--- overflow-util.py
+++ overflow-util.py
@@ -5,12 +5,28 @@ def printHelpInfo():
     print "COMMANDS\n"
     print " Repeat a string n times and print it:"
     print "  overflow-util.py repeat <string to repeat> <number of times>"
+    print ""
+    print " Convert an ASCII string to hexadecimal:"
+    print "  overflow-util.py asciiToHex <string to convert>"
+    print ""
+    print " Convert a hexadecimal to ASCII:"
+    print "  overflow-util.py hexToAscii <hexadecimal number (without \"0x\" prefix)>"
 
 def repeatString(stringToRepeat, n):
     output = stringToRepeat
     for i in range(1, n):
         output += stringToRepeat
     return output
+
+def asciiToHex(asciiString):
+    hexString = "0x"
+    for c in asciiString:
+        hexString += str(hex(ord(c)))[2:]
+
+    return hexString
+
+def hexToAscii(hexString):
+    return "".join(chr(int(hexString[i:i+2], 16)) for i in xrange(0, len(hexString), 2))
 
 if len(sys.argv) == 1:
     print "You need to provide some arguments. Try help"
@@ -28,3 +44,7 @@ else:
                 print "You need to supply an integer for the number of repetitions."
         else:
             print "You need to supply a string to repeat and the number of times to repeat it."
+    if func == "asciiToHex":
+        print asciiToHex(sys.argv[2])
+    if func == "hexToAscii":
+        print hexToAscii(sys.argv[2])
